5c25c1a3b2f5b96a47113baa483f1fe71c5009db
Yan-Santana <yansantana63@gmail.com>
2023-11-05 15:01:55 -0400

    REFACTOR: update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import psycopg2
 import csv
 
-connect = psycopg2.connect(database="SRAG", user="postgres", password="postgres123", host="localhost", port="5432")
+connect = psycopg2.connect (
+    database="SRAG",
+    user="postgres",
+    password="minhasenha", 
+    host="meucontainer", 
+    port="5433"
+)
 
-
-with open('INFLUD20-01-05-2023.csv', 'r') as file:
+with open('C:\Users\Yan-Santana\Downloads\INFLUD20-01-05-2023.csv', 'r') as file:
     reader = csv.reader(file)
-    next(reader) # Pula a primeira linha
+    next(reader) 
 
     for row in reader:
         cur = connect.cursor()
         
+    # ESTOU ARRUMANDO ESSA PARTE AQUI! 
         query = "INSERT INTO dados (data, estado, municipio, obito, sexo, idade, raca, evolucao, dt_notific, dt_inicio_sintomas, dt_obito, classificacao_final, criterio_confirmacao, criterio_obito, status_notificacao, municipio_notificacao) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
         row[3] = int (row[3])
